fcd37b3583d5e5bf97d5b2c9d507cedf4970b7d4
Nick Manos <nmanos@student.bridgew.edu>
2024-01-28 19:22:19 -0500

    WIP: Main.kt

--- src/main/kotlin/edu/bridgew/comp430/proj1/Main.kt
+++ src/main/kotlin/edu/bridgew/comp430/proj1/Main.kt
@@ -1,23 +1,38 @@
 package edu.bridgew.comp430.proj1
 
+import edu.bridgew.comp430.proj1.api.GoogleJobSearchServiceImpl
 import edu.bridgew.comp430.proj1.api.SerpApiClient
+import edu.bridgew.comp430.proj1.api.data.Job
+import edu.bridgew.comp430.proj1.api.data.UnknownExtension
 import io.github.cdimascio.dotenv.dotenv
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 private val dotenv = dotenv {
     ignoreIfMissing = false
 }
 
-fun main(args: Array<String>) = runBlocking {
-    val jobSearchClient = SerpApiClient(dotenv["JOBSPROJ_API_KEY"]).jobSearchApi
-    val jobs = async {
-        jobSearchClient.search("software engineer")
-    }
-    println(jobs.await())
-}
+fun main(args: Array<String>): Unit = runBlocking {
+    val retrofit = SerpApiClient(dotenv["JOBSPROJ_API_KEY"]).retrofit
+    val jobSearchClient = GoogleJobSearchServiceImpl(retrofit)
+//    val jobSearch = withContext(Dispatchers.IO) {
+//        val jobs = mutableListOf<Job>()
+//            (0..<5).map { page ->
+//                async {
+//                    jobSearchClient.search("software engineer", page * 10).jobsResults
+//                }
+//            }
+//        }
+//
+//    val jobs: List<Job> = jobSearch.awaitAll().flatMap { mutableListOf<Job>().apply {
+//        if (it != null) {
+//            addAll(it)
+//        }
+//    } }
 
-//fun main(args: Array<String>) {
-//    println(dotenv["JOBSPROJ_API_KEY"])
-//}
+    val jobs = jobSearchClient.getJobsPages("software engineer", 2)
+
+    jobs.forEach { println(it); println() }
+
+    val unknownExtensions = jobs.flatMap { it.detectedExtensions.filterIsInstance<UnknownExtension>() }
+    unknownExtensions.forEach { println(it) }
+}
