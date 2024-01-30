80b204c90303a43fdc84613d31f535e6a21f5166
Nick Manos <nmanos@student.bridgew.edu>
2024-01-29 23:36:23 -0500

    Refactor API call to use Kotlin flow

--- src/main/kotlin/edu/bridgew/comp430/proj1/Main.kt
+++ src/main/kotlin/edu/bridgew/comp430/proj1/Main.kt
@@ -1,40 +1,38 @@
 package edu.bridgew.comp430.proj1
 
+import edu.bridgew.comp430.proj1.api.ApiResult
 import edu.bridgew.comp430.proj1.api.GoogleJobSearchServiceImpl
 import edu.bridgew.comp430.proj1.api.SerpApiClient
 import edu.bridgew.comp430.proj1.api.data.UnknownExtension
 import io.github.cdimascio.dotenv.dotenv
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.runBlocking
 
 private val dotenv = dotenv {
     ignoreIfMissing = false
 }
 
+@OptIn(ExperimentalCoroutinesApi::class)
 fun main(args: Array<String>): Unit = runBlocking {
     val retrofit = SerpApiClient(dotenv["JOBSPROJ_API_KEY"]).retrofit
     val jobSearchClient = GoogleJobSearchServiceImpl(retrofit)
-//    val jobSearch = withContext(Dispatchers.IO) {
-//        val jobs = mutableListOf<Job>()
-//            (0..<5).map { page ->
-//                async {
-//                    jobSearchClient.search("software engineer", page * 10).jobsResults
-//                }
-//            }
-//        }
-//
-//    val jobs: List<Job> = jobSearch.awaitAll().flatMap { mutableListOf<Job>().apply {
-//        if (it != null) {
-//            addAll(it)
-//        }
-//    } }
+    val pages = 3
 
-    val jobs = jobSearchClient.getJobsPages("software engineer", 2)
-
-    jobs.forEach {
-        println(it)
-        println()
-    }
-
-    val unknownExtensions = jobs.flatMap { it.detectedExtensions.filterIsInstance<UnknownExtension>() }
-    unknownExtensions.forEach { println(it) }
+    (0..<3).asFlow()
+        .flatMapMerge { page -> jobSearchClient.getJobs("software engineer", page) }
+        .buffer(pages)
+        .collect { result ->
+            when (result) {
+                is ApiResult.Success -> {
+                    result.body.forEach {
+                        println(it)
+                        println("===================================")
+                    }
+                }
+                is ApiResult.Error -> println("ERROR! ${result.errorBody}")
+            }
+        }
 }
--- src/main/kotlin/edu/bridgew/comp430/proj1/api/GoogleJobSearchServiceImpl.kt
+++ src/main/kotlin/edu/bridgew/comp430/proj1/api/GoogleJobSearchServiceImpl.kt
@@ -2,16 +2,31 @@ package edu.bridgew.comp430.proj1.api
 
 import edu.bridgew.comp430.proj1.api.data.Job
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.withContext
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.create
 
 class GoogleJobSearchServiceImpl(retrofit: Retrofit) : GoogleJobSearchService by retrofit.create() {
-    suspend fun getJobsPages(query: String, pages: Int = 1): List<Job> = withContext(Dispatchers.IO) {
-        return@withContext (0..<pages).map { page -> async { search(query, page * 10) } }
-            .awaitAll()
-            .flatMap { it.body()?.jobsResults ?: listOf() }
-    }
+    suspend fun getJobs(query: String, page: Int = 0): Flow<ApiResult<List<Job>>> = flow {
+        // page -> search(query, page * 10).body()?.jobsResults?.asFlow()
+        val response = search(query, page * 10)
+
+        if (response.isSuccessful) {
+            val jobList = response.body()?.jobsResults!!
+            emit(ApiResult.Success(jobList))
+        } else {
+            emit(ApiResult.Error(response.code(), response.errorBody()))
+        }
+    }.flowOn(Dispatchers.IO)
 }
