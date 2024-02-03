2b3a9613e9362ae96471fa8ae316588b07d1f380
Nick Manos <nmanos@student.bridgew.edu>
2024-02-03 18:04:37 -0500

    Rework Extension entity for database

--- src/main/kotlin/edu/bridgew/comp490/proj1/data/entities/Extension.kt
+++ src/main/kotlin/edu/bridgew/comp490/proj1/data/entities/Extension.kt
@@ -2,7 +2,19 @@ package edu.bridgew.comp490.proj1.data.entities
 
 import java.time.LocalDateTime
 
-sealed class Extension(val id: Int)
+sealed class Extension(val id: Int) {
+    companion object {
+        @JvmStatic
+        fun getById(id: Int, value: String): Extension {
+            return when (id) {
+                1 -> ScheduleType(value)
+                2 -> PostedAt(LocalDateTime.parse(value))
+                3 -> Salary(value)
+                else -> throw IllegalArgumentException("id value expected to be in (1, 2, 3), got $id")
+            }
+        }
+    }
+}
 
 data class ScheduleType(val type: String) : Extension(1)
 data class PostedAt(val date: LocalDateTime) : Extension(2)
