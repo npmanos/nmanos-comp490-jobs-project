9dc0e1eecb0b65a9a1e13a7d6a86c530306dd8a7
Nick Manos <nmanos@student.bridgew.edu>
2024-02-27 01:02:51 -0500

    Shrink jobs list in preparation for list-detail layout

--- src/main/kotlin/edu/bridgew/comp490/proj1/Utils.kt
+++ src/main/kotlin/edu/bridgew/comp490/proj1/Utils.kt
@@ -3,6 +3,7 @@ package edu.bridgew.comp490.proj1
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
@@ -92,3 +93,5 @@ fun HorizontalSpacer(width: Dp) = Spacer(modifier = Modifier.width(width))
 
 @Composable
 fun VerticalSpacer(height: Dp) = Spacer(modifier = Modifier.height(height))
+
+typealias MaterialIcons = Icons.Rounded
--- src/main/kotlin/edu/bridgew/comp490/proj1/ui/screen/JobListScreen.kt
+++ src/main/kotlin/edu/bridgew/comp490/proj1/ui/screen/JobListScreen.kt
@@ -2,11 +2,11 @@ package edu.bridgew.comp490.proj1.ui.screen
 
 import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,24 +15,26 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Domain
 import androidx.compose.material.icons.rounded.LocationOn
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.uniqueScreenKey
 import edu.bridgew.comp490.proj1.HorizontalSpacer
+import edu.bridgew.comp490.proj1.MaterialIcons
 import edu.bridgew.comp490.proj1.data.entities.Job
 import edu.bridgew.comp490.proj1.ui.screenmodel.JobListScreenModel
 import edu.bridgew.comp490.proj1.ui.screenmodel.JobListScreenModel.State
@@ -46,12 +48,14 @@ data class JobListScreen(private val screenModel: JobListScreenModel) : Screen {
         val screenModel = rememberScreenModel { screenModel }
         val state by screenModel.state.collectAsState()
 
-        Box(
-            modifier = Modifier.fillMaxSize()
+        Surface(
+            modifier = Modifier.fillMaxHeight().fillMaxWidth(1f / 3f),
+            shape = MaterialTheme.shapes.large,
+            color = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.contentColorFor(MaterialTheme.colorScheme.background)
         ) {
 
             val listState = rememberLazyListState()
-
             when (state) {
                 is State.Init -> {
                     LoadingJobs()
@@ -59,7 +63,7 @@ data class JobListScreen(private val screenModel: JobListScreenModel) : Screen {
                 }
 
                 is State.Loading -> LoadingJobs()
-                is State.Result -> {
+                is State.Result -> Box(modifier = Modifier.fillMaxSize()) {
                     LazyColumn(
                         modifier = Modifier.fillMaxSize(),
                         contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
@@ -70,50 +74,7 @@ data class JobListScreen(private val screenModel: JobListScreenModel) : Screen {
                             items = jobs,
                             key = { it.jobId },
                         ) { job ->
-                            //JobListItem(job)
-                            ListItem(
-                                headlineContent = {
-                                    Text(
-                                        text = job.title,
-                                        fontSize = 16.sp,
-                                        lineHeight = 24.0.sp,
-//                                        modifier = Modifier.padding(bottom = 4.dp)
-                                    )
-                                },
-                                supportingContent = {
-                                    Row(verticalAlignment = Alignment.CenterVertically) {
-                                        Icon(
-                                            painter = painterResource("/edu/bridgew/comp490/proj1/screen/domain-24px.xml"),
-                                            contentDescription = null,
-                                            modifier = Modifier.size(17.dp).alignByBaseline(),
-                                        )
-                                        HorizontalSpacer(4.dp)
-                                        Text(
-                                            text = job.companyName.trim(),
-                                            fontSize = 14.sp,
-                                            lineHeight = 20.0.sp,
-                                            modifier = Modifier.alignByBaseline(),
-                                        )
-
-                                        if (job.location != null) {
-                                            HorizontalSpacer(12.dp)
-                                            Icon(
-                                                imageVector = Icons.Rounded.LocationOn,
-                                                contentDescription = null,
-                                                modifier = Modifier.size(17.dp).alignByBaseline(),
-                                            )
-                                            HorizontalSpacer(4.dp)
-                                            Text(
-                                                text = job.location.trim(),
-                                                fontSize = 14.sp,
-                                                lineHeight = 20.0.sp,
-                                                modifier = Modifier.alignByBaseline(),
-                                            )
-                                        }
-                                    }
-                                },
-                                colors = ListItemDefaults.colors()
-                            )
+                            JobListItem(job)
 
                             if (job != jobs.last()) {
                                 Divider()
@@ -123,7 +84,7 @@ data class JobListScreen(private val screenModel: JobListScreenModel) : Screen {
 
                     VerticalScrollbar(
                         modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight().padding(end = 2.dp, top = 2.dp, bottom = 2.dp),
-                        adapter = rememberScrollbarAdapter(listState)
+                        adapter = rememberScrollbarAdapter(listState),
                     )
                 }
             }
@@ -140,41 +101,50 @@ data class JobListScreen(private val screenModel: JobListScreenModel) : Screen {
     }
 
     @Composable
-    private fun JobListItem(job: Job) {
-        Box {
-            Column {
-                Text(
-                    text = job.title,
-                    fontSize = 20.sp,
-                    modifier = Modifier.padding(bottom = 4.dp)
+    private fun JobListItem(job: Job) = ListItem(
+        headlineContent = {
+            Text(
+                text = job.title,
+                style = MaterialTheme.typography.bodyLarge,
+            )
+        },
+        supportingContent = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    imageVector = MaterialIcons.Domain,
+                    contentDescription = null,
+                    modifier = Modifier.size(17.dp).alignByBaseline(),
                 )
-                Row(verticalAlignment = Alignment.CenterVertically) {
+                HorizontalSpacer(4.dp)
+                Text(
+                    text = job.companyName.trim(),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.alignByBaseline(),
+                )
+
+                if (job.location != null) {
+                    HorizontalSpacer(12.dp)
                     Icon(
-                        painter = painterResource("/edu/bridgew/comp490/proj1/screen/domain-24px.xml"),
+                        imageVector = MaterialIcons.LocationOn,
                         contentDescription = null,
-                        modifier = Modifier.size(16.dp),
+                        modifier = Modifier.size(17.dp).alignByBaseline(),
                     )
                     HorizontalSpacer(4.dp)
                     Text(
-                        text = job.companyName.trim(),
-                        fontSize = 16.sp
+                        text = job.location.trim(),
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.alignByBaseline(),
                     )
-
-                    if (job.location != null) {
-                        HorizontalSpacer(12.dp)
-                        Icon(
-                            imageVector = Icons.Rounded.LocationOn,
-                            contentDescription = null,
-                            modifier = Modifier.size(16.dp),
-                        )
-                        HorizontalSpacer(4.dp)
-                        Text(
-                            text = job.location.trim(),
-                            fontSize = 16.sp
-                        )
-                    }
                 }
             }
-        }
-    }
+        },
+        colors = JobListItemColors(selected = false)
+    )
+
+    @Composable
+    private fun JobListItemColors(selected: Boolean) = ListItemDefaults.colors(
+        containerColor = MaterialTheme.colorScheme.background,
+        headlineColor = contentColorFor(MaterialTheme.colorScheme.background),
+        supportingColor = contentColorFor(MaterialTheme.colorScheme.background),
+    )
 }
