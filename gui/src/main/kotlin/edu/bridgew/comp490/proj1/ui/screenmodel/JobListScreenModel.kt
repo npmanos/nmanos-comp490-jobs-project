ee9038964b671b811962b03b8dd653c289a43c6f
Nick Manos <nmanos@student.bridgew.edu>
2024-02-29 18:24:35 -0500

    Sort job list by date

--- gui/src/main/kotlin/edu/bridgew/comp490/proj1/ui/screenmodel/JobListScreenModel.kt
+++ gui/src/main/kotlin/edu/bridgew/comp490/proj1/ui/screenmodel/JobListScreenModel.kt
@@ -1,9 +1,12 @@
 package edu.bridgew.comp490.proj1.ui.screenmodel
 
+import androidx.compose.ui.util.fastFirstOrNull
 import cafe.adriel.voyager.core.model.StateScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
 import edu.bridgew.comp490.proj1.data.JobRepository
 import edu.bridgew.comp490.proj1.data.entities.Job
+import edu.bridgew.comp490.proj1.data.entities.PostedAt
+import edu.bridgew.comp490.proj1.relativeTimeString
 import kotlinx.coroutines.launch
 import java.io.Serializable
 
@@ -20,10 +23,20 @@ class JobListScreenModel(private val repository: JobRepository) : StateScreenMod
         data class Result(val jobs: List<Job>) : State()
     }
 
+    private val dateComparator = compareBy<Job> { job ->
+        val postedAt = job.detectedExtensions?.fastFirstOrNull { it is PostedAt } as PostedAt?
+
+        postedAt?.date?.toLocalDate()
+    }.reversed().thenBy { job ->
+        val postedAt = job.detectedExtensions?.fastFirstOrNull { it is PostedAt } as PostedAt?
+
+        postedAt?.date?.relativeTimeString
+    }.thenBy { job -> job.title }
+
     fun getJobs() {
         screenModelScope.launch {
             mutableState.value = State.Loading
-            mutableState.value = State.Result(repository.getJobs())
+            mutableState.value = State.Result(repository.getJobs().sortedWith(dateComparator))
         }
     }
 }
