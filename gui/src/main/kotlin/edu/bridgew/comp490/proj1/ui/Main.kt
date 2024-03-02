2bb8f37a25006289776da084b6bffc8c3cb618d0
Nick Manos <nmanos@student.bridgew.edu>
2024-03-02 01:20:26 -0500

    Set minimum window size

--- gui/src/main/kotlin/edu/bridgew/comp490/proj1/ui/Main.kt
+++ gui/src/main/kotlin/edu/bridgew/comp490/proj1/ui/Main.kt
@@ -3,6 +3,8 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -12,14 +14,9 @@ import cafe.adriel.voyager.navigator.Navigator
 import edu.bridgew.comp490.proj1.ui.di.guiModule
 import edu.bridgew.comp490.proj1.ui.screen.JobListScreen
 import edu.bridgew.comp490.proj1.ui.theme.BsuTheme
-import io.github.cdimascio.dotenv.dotenv
 import org.koin.compose.KoinApplication
 import org.koin.core.logger.Level
-
-private val dotenv = dotenv {
-    ignoreIfMissing = true
-    ignoreIfMalformed = true
-}
+import java.awt.Dimension
 
 @Preview
 @Composable
@@ -42,6 +39,12 @@ fun main() = application {
             onCloseRequest = ::exitApplication,
             state = state
         ) {
+            // Min window size code from https://github.com/JetBrains/compose-multiplatform/issues/2285#issuecomment-1873001531
+            with (LocalDensity.current) {
+                val minSize = DpSize(1280.dp, 720.dp).toSize()
+                window.minimumSize = Dimension(minSize.width.toInt(), minSize.height.toInt())
+            }
+
             BsuTheme { App() }
         }
     }
